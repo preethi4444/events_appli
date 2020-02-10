6c787dc8b6cbf9b15d06b98c9205d89b67edbee6
SaiPreethi Induri <Saipreethi_Induri@epam.com>
2020-02-10 12:54:38 +0530

    Add project files.

---
+++ events_appli/Global.asax.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Optimization;
+using System.Web.Routing;
+using System.Web.Security;
+using System.Web.SessionState;
+
+namespace events_appli
+{
+    public class Global : HttpApplication
+    {
+        void Application_Start(object sender, EventArgs e)
+        {
+            // Code that runs on application startup
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+            Application["totalapplications"] = 0;
+            Application["totalusers"] = 0;
+            Application["totalapplications"] = (int)Application["totalapplications"] + 1;
+        }
+        void Session_Start(object sender, EventArgs e)
+        {
+            Application["totalusers"] = (int)Application["totalusers"] + 1;
+        }
+        void Session_End(object sender, EventArgs e)
+        {
+            Application["totalusers"] = (int)Application["totalusers"] -1;
+        }
+    }
+}
